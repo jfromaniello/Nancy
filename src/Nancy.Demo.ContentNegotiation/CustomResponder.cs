e9b00a057d7e2d913b94ea3192a6f01824227de5
Jose Romaniello <jfromaniello@gmail.com>
2011-04-17 16:11:07 -0300

    added a CustomResponder to the ContentNegotiation example

---
+++ src/Nancy.Demo.ContentNegotiation/CustomResponder.cs
@@ -0,0 +1,32 @@
+﻿namespace Nancy.Demo.ContentNegotiation
+{
+    using System.Xml.Serialization;
+    using Nancy.ContentNegotiation;
+    using Responses;
+
+    public class CustomResponder : IResponder
+    {
+        public const string MyCompanyMediaType = "application/vnd.MyCompany.xml";
+
+        public bool CanRespond(Request request, object value)
+        {
+
+            return request.Accept(MyCompanyMediaType) 
+                   && value.GetType().Equals(typeof (TestModel));
+        }
+
+        public Response CreateResponse(Request request, IResponseFormatter responseFormatter, object value)
+        {
+            var casted = (TestModel) value;
+            var alternativeModel = new AlternativeModel {Name = casted.FirstName, LastName = casted.LastName};
+            return new XmlResponse<AlternativeModel>(alternativeModel, MyCompanyMediaType);
+        }
+
+        [XmlRoot(ElementName = "Person")]
+        public class AlternativeModel
+        {
+            public string Name { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
--- src/Nancy.Demo.ContentNegotiation/Program.cs
+++ src/Nancy.Demo.ContentNegotiation/Program.cs
@@ -3,15 +3,36 @@
     using System;
     using Hosting.Self;
 
+    /// <summary>
+    /// the only purpose of this bootstraper is to register the CustomResponder.
+    /// </summary>
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void InitialiseInternal(TinyIoC.TinyIoCContainer container)
+        {
+            base.InitialiseInternal(container);
+            container.Register(typeof(CustomResponder));
+        }
+    }
+
     class Program
     {
         static void Main()
         {
-            var nancyHost = new NancyHost(new Uri("http://localhost:1234/nancy/"));
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            var nancyHost = new NancyHost(new Uri("http://localhost:1234/nancy/"), new Bootstrapper());
+            
             nancyHost.Start();
 
-            var contentTypes = string.Join( "," ,new[] {"application/xml", "application/json", "text/xml", "text/json"});
-            Console.WriteLine("You can issue requests to http://localhost:1234/nancy/ with fiddler and use Content-Type: {0} and see the different responses.", contentTypes);
+
+            var message = string.Format("You can issue requests to http://localhost:1234/nancy/ with fiddler \n"
+                       + "and use Content-Type 'text/xml' or 'text/json' and see the different responses. \n"
+                       + "This demo also has a custom responder, if you use {0} you will see \n" 
+                       + "a different xml version of the resource.", 
+                       CustomResponder.MyCompanyMediaType);
+
+            Console.WriteLine(message);
             Console.ReadKey();
 
             nancyHost.Stop();
@@ -19,4 +40,6 @@
             Console.WriteLine("Stopped. Good bye!");
         }
     }
+
+   
 }
